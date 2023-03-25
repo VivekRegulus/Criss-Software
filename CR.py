e210d6463a24bb37990a5cfb6ff1a3e0ec412eca
VivekRegulus <128822236+VivekRegulus@users.noreply.github.com>
2023-03-25 15:17:55 +0530

    Add files via upload

---
+++ CR.py
@@ -0,0 +1,16 @@
+import socket
+
+serverIP = "172.17.29.11"
+serverPORT = 6000
+
+serveraddress = (serverIP, serverPORT)
+bufferSize = 1024
+
+UDPClientSocket = socket.socket (family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+message = "Hi, this is Vivek Raj Barnwal"
+
+bytestosend = str.encode(message)
+
+UDPClientSocket.sendto(bytestosend, serveraddress)
+
